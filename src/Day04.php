f4aa4f3cf8a6c46272971ac42144fb7cafd2b7b5
Leif Gehrmann <leif.gehrmann@gmail.com>
2023-12-03 22:16:40 +0000

    Day 04 - Outline

---
+++ src/Day04.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aoc;
+
+use Exception;
+
+/**
+ * The class is used, it's just called dynamically from App.php.
+ * @psalm-suppress UnusedClass
+ */
+class Day04 extends AbstractDay
+{
+    /**
+     * @return int
+     */
+    public function solvePart1(): int
+    {
+        return 0;
+    }
+
+    public function solve(): array
+    {
+        return [
+            "Part 1" => $this->solvePart1(),
+        ];
+    }
+}
