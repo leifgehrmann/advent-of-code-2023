1b7b112b920e214435b4537c2b133fd903b6f1c3
Leif Gehrmann <leif.gehrmann@gmail.com>
2023-12-07 21:11:30 +0000

    Day 07 - Part 2 Solved

--- src/Day07.php
+++ src/Day07.php
@@ -36,12 +36,40 @@ class Day07 extends AbstractDay
         return $sum;
     }
 
+    /**
+     * @param HandBid[] $puzzle
+     * @return int
+     */
+    public function solvePart2(array $puzzle): int
+    {
+        usort($puzzle, function ($handBidA, $handBidB) {
+            return $this->compareHandWithJokerRule($handBidA['hand'], $handBidB['hand']);
+        });
+        $sum = 0;
+        foreach ($puzzle as $order => $handBid) {
+            $sum += ($order + 1) * $handBid['bid'];
+        }
+        return $sum;
+    }
+
     public function compareHand(string $handA, string $handB): int
     {
         $handAType = $this->getHandType($handA);
         $handBType = $this->getHandType($handB);
         if ($handAType === $handBType) {
             return $this->compareHandStrength($handA, $handB);
+        } elseif ($handAType > $handBType) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public function compareHandWithJokerRule(string $handA, string $handB): int
+    {
+        $handAType = $this->getHandTypeWithJokerRule($handA);
+        $handBType = $this->getHandTypeWithJokerRule($handB);
+        if ($handAType === $handBType) {
+            return $this->compareHandStrengthWithJokerRule($handA, $handB);
         } elseif ($handAType > $handBType) {
             return 1;
         }
@@ -93,6 +121,69 @@ class Day07 extends AbstractDay
         };
     }
 
+    public function getHandTypeWithJokerRule(string $hand): int
+    {
+        $counts = count_chars($hand);
+        $jokerCount = $counts[ord('J')];
+        $counts = array_filter($counts);
+
+        if ($jokerCount === 5) {
+            return self::FIVE_OF_A_KIND;
+        } elseif ($jokerCount === 4) {
+            return self::FIVE_OF_A_KIND;
+        } elseif ($jokerCount === 3) {
+            if (in_array(2, $counts)) {
+                return self::FIVE_OF_A_KIND; // JJJAA
+            }
+            return self::FOUR_OF_A_KIND; // JJJAB
+        } elseif ($jokerCount === 2) {
+            if (in_array(3, $counts)) {
+                return self::FIVE_OF_A_KIND; // JJAAA
+            } elseif (count($counts) === 3) {
+                return self::FOUR_OF_A_KIND; // JJAAB
+            }
+            return self::THREE_OF_A_KIND; // JJABC;
+        } elseif ($jokerCount === 1) {
+            if (in_array(4, $counts)) {
+                return self::FIVE_OF_A_KIND; // JAAAA
+            } elseif (in_array(3, $counts)) {
+                return self::FOUR_OF_A_KIND; // JAAAB
+            } elseif (in_array(2, $counts) && count($counts) === 3) {
+                return self::FULL_HOUSE; // JAABB
+            } elseif (in_array(2, $counts)) {
+                return self::THREE_OF_A_KIND; // JAABC
+            }
+            return self::ONE_PAIR; // JABCD
+        }
+        return self::getHandType($hand);
+    }
+
+    public function compareHandStrengthWithJokerRule(string $handA, string $handB): int
+    {
+        for ($i = 0; $i < 5; $i++) {
+            if ($handA[$i] === $handB[$i]) {
+                continue;
+            }
+            if ($this->getCardStrengthWithJokerRule($handA[$i]) > $this->getCardStrengthWithJokerRule($handB[$i])) {
+                return 1;
+            }
+            return -1;
+        }
+        return 0;
+    }
+
+    public function getCardStrengthWithJokerRule(string $card): int
+    {
+        return match ($card) {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 1,
+            'T' => 10,
+            default => intval($card),
+        };
+    }
+
     /**
      * @return HandBid[]
      */
@@ -112,6 +203,7 @@ class Day07 extends AbstractDay
 
         return [
             "Part 1" => $this->solvePart1($puzzle),
+            "Part 2" => $this->solvePart2($puzzle),
         ];
     }
 }
--- tests/Unit/Day07Test.php
+++ tests/Unit/Day07Test.php
@@ -15,6 +15,14 @@ final class Day07Test extends TestCase
         $this->assertSame(6440, $dayPuzzle->solvePart1($puzzle));
     }
 
+    public function testPart2ExampleInput(): void
+    {
+        $dayPuzzle = new Day07();
+        $input = Helper::getSampleData('Day07Sample.data');
+        $puzzle = $dayPuzzle->parsePuzzle($input);
+        $this->assertSame(5905, $dayPuzzle->solvePart2($puzzle));
+    }
+
     /**
      * @dataProvider providerCompareHandStrength
      */
@@ -51,6 +59,56 @@ final class Day07Test extends TestCase
             ['23432', Day07::TWO_PAIR],
             ['A23A4', Day07::ONE_PAIR],
             ['12345', Day07::HIGH_CARD],
+        ];
+    }
+
+    /**
+     * @dataProvider providerCompareHandWithJokerRule
+     */
+    public function testCompareHandWithJokerRule(string $handA, string $handB, $expected): void
+    {
+        $dayPuzzle = new Day07();
+        $this->assertSame($expected, $dayPuzzle->compareHandWithJokerRule($handA, $handB));
+    }
+
+    public static function providerCompareHandWithJokerRule(): array
+    {
+        return [
+            ['KTJJT', 'QQQJA', 1],
+            ['QQQJA', 'T55J5', 1],
+            ['T55J5', 'KK677', 1],
+            ['KK677', 'T3K', 1],
+        ];
+    }
+
+    /**
+     * @dataProvider providerGetHandTypeWithJokerRule
+     */
+    public function testGetHandTypeWithJokerRule(string $hand, $expected): void
+    {
+        $dayPuzzle = new Day07();
+        $this->assertSame($expected, $dayPuzzle->getHandTypeWithJokerRule($hand));
+    }
+
+    public static function providerGetHandTypeWithJokerRule(): array
+    {
+        return [
+            ['32T3K', Day07::ONE_PAIR],
+            ['KK677', Day07::TWO_PAIR],
+            ['T55J5', Day07::FOUR_OF_A_KIND],
+            ['KTJJT', Day07::FOUR_OF_A_KIND],
+            ['QQQJA', Day07::FOUR_OF_A_KIND],
+            ['JJJJJ', Day07::FIVE_OF_A_KIND],
+            ['JJJJA', Day07::FIVE_OF_A_KIND],
+            ['JJJAA', Day07::FIVE_OF_A_KIND],
+            ['JJJAB', Day07::FOUR_OF_A_KIND],
+            ['JJAAA', Day07::FIVE_OF_A_KIND],
+            ['JJAAB', Day07::FOUR_OF_A_KIND],
+            ['JJABC', Day07::THREE_OF_A_KIND],
+            ['JAAAA', Day07::FIVE_OF_A_KIND],
+            ['JAAAB', Day07::FOUR_OF_A_KIND],
+            ['JAABC', Day07::THREE_OF_A_KIND],
+            ['JABCD', Day07::ONE_PAIR],
         ];
     }
 }
